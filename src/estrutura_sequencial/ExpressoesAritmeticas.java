8fa5d47c786e9a3fcda531ab4d1cdae9bb30f2f6
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2022-12-28 09:55:51 -0300

    expressões aritmeticas

---
+++ src/estrutura_sequencial/ExpressoesAritmeticas.java
@@ -0,0 +1,19 @@
+package estrutura_sequencial;
+
+public class ExpressoesAritmeticas {
+
+	public static void main(String[] args) {
+		
+		// 2 * 6 / 3             Resultado = 4 
+	    // 3 + 2 * 4             Resultado = 11
+		// (3 + 2) * 4           Resultado = 20
+		// 60 / (3 + 2) * 4      Resultado = 48
+		// 60 / ((3 + 2) * 4)    Resultado = 3
+		
+		// operador MOD
+		// 14 % 3                Resultado = 2, pois 14 % 3 = 4, sobra 2
+		// 19 % 5                Resultado = 4, pois 19 % 5 = 3, sobra 4
+		
+	}
+
+}
