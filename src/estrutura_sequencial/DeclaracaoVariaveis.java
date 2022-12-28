6d979a0666ffb5ef8a7f8f3a42c77b13a0e77de8
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2022-12-28 16:46:14 -0300

    declarando variavéis

---
+++ src/estrutura_sequencial/DeclaracaoVariaveis.java
@@ -0,0 +1,14 @@
+package estrutura_sequencial;
+
+public class DeclaracaoVariaveis {
+
+	public static void main(String[] args) {
+		// <tipo> <nome> = <valor inicial>
+		// exemplo 
+		int idade = 25; // para números inteiros
+		double altura = 1.58; // para números decimais
+		char sexo = 'F'; // para uma única letra 
+		boolean verdade = false;
+	}
+
+}
