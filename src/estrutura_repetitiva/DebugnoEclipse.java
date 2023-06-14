534605b4bdaccd2a8094008b4072463e7ba54880
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2023-06-14 19:07:44 -0300

    arquivo usado para fazer debug

---
+++ src/estrutura_repetitiva/DebugnoEclipse.java
@@ -0,0 +1,27 @@
+package estrutura_repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DebugnoEclipse {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("Área = %.2f%n", area);
+		System.out.printf("Preço = %.2f%n", preco);
+		
+		sc.close();
+
+	}
+
+}
